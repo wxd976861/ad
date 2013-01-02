9336433f656149ca152d7e5569d50feea19b7638
donnie <donnie4w@gmail.com>
2013-01-02 11:30:43 +0800

    test

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		System.out.println(new Date());
+		System.out.println(new Date());
 	}
 }
