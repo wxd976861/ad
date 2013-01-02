e8541405e20fe677a017334fe8daceb484acecc8
donnie <donnie4w@gmail.com>
2013-01-02 11:52:17 +0800

    Test2

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("This is the second test class");
+		System.out.println("This is the second test class2");
 	}
 }
